d6efa81082eb07ceb3ccc02ae7fd036df324cc90
Danish <tux.danish@gmail.com>
2025-02-19 18:03:33 +0400

    use token from env

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -54,7 +54,7 @@ const domain = process.env.SHOPIFY_STORE_DOMAIN
   ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://')
   : '';
 const endpoint = `${domain}${SHOPIFY_GRAPHQL_API_ENDPOINT}`;
-const name = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+const name = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN ?? "";
 
 type ExtractVariables<T> = T extends { variables: object } ? T['variables'] : never;
 export async function shopifyFetch<T>({
