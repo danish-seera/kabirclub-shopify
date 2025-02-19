2ab886a4326df4b3df3f246fbfb8b78c954c1961
Danish <tux.danish@gmail.com>
2025-02-19 14:50:16 +0400

    bypass nextjs restriction

--- next.config.js
+++ next.config.js
@@ -23,5 +23,8 @@ module.exports = {
         permanent: true
       }
     ];
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   }
 };
---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+    return <div className="w-screen h-screen">
+        Page is not found
+    </div>
+}
