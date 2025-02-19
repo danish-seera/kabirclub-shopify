b39ba21b8a3b8edd6d4cef4eea87fb71595fd00b
Danish <tux.danish@gmail.com>
2025-02-19 14:09:45 +0400

    ds

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -54,7 +54,7 @@ const domain = process.env.SHOPIFY_STORE_DOMAIN
   ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://')
   : '';
 const endpoint = `${domain}${SHOPIFY_GRAPHQL_API_ENDPOINT}`;
-const key = "8274df384db772a5d0b917869fb49dcc";
+const name = "8274df384db772a5d0b917869fb49dcc";
 
 type ExtractVariables<T> = T extends { variables: object } ? T['variables'] : never;
 export async function shopifyFetch<T>({
@@ -76,7 +76,7 @@ export async function shopifyFetch<T>({
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token': key,
+        'X-Shopify-Storefront-Access-Token': name,
         ...headers
       },
       body: JSON.stringify({
